e96845845ee363077d01a9c956608692e4b5e7c1
SerhiiRuban81 <serhii.ruban@ukr.net>
2023-11-01 00:37:43 +0200

    view component for categories menu and menu styling added

---
+++ Makeup#1/ViewComponents/CategoriesMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Makeup_1.Database;
+using MakeupClassLibrary.DomainModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Makeup_1.ViewComponents
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private readonly ShopContext context;
+
+        public CategoriesMenuViewComponent(ShopContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Category> categories = await context.Categories.Where(c=>c.ParentCategory == null). Include(t => t.ChildCategories)!.ThenInclude(a=>a.ChildCategories).ToListAsync();
+            return View(categories);
+        }
+    }
+}
