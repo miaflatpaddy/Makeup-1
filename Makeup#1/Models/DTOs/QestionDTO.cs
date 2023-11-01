8c8a8cf3b94166aaffa565145c5d8920f1948786
miaflatpaddy <kurisumakise33@gmail.com>
2023-11-02 02:06:26 +0300

    вопросы (не до конца)

--- Makeup#1/Models/DTOs/QestionDTO.cs
+++ Makeup#1/Models/DTOs/QestionDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace Makeup_1.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Makeup_1.Models.DTOs
 {
     public class QestionDTO
     {
+        [Display(Name = "Заголовок")]
+        public string title { get; set; }
+        [Display(Name = "Питання")]
+        public string content { get; set; }
+        public int productId { get; set; }
+        public string returnUrl { get; set; }
     }
 }
--- MakeupClassLibrary/DomainModels/Question.cs
+++ MakeupClassLibrary/DomainModels/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace MakeupClassLibrary.DomainModels
 {
     internal class Question
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = default!;
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        public Product Products { get; set; } = default!;
+
+        public User User { get; set; } = default!;
+        public DateTime Created { get; set; } = DateTime.Now;
+
     }
 }
